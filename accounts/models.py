c86b52b9953ba32deb99dd14076c5a976d1c5960
Luis Alberto Poblete <beto_ero@hotmail.com>
2017-04-05 11:39:36 -0500

    Creación de app accounts

---
+++ accounts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+
+# Create your models here.
+class Profile (models.Model):
+	user_profile = models.OnetoOneField(settings.AUTH_USER_MODEL)
+	photo = models.ImageField(upload_to='users', blank=True)
+	tel = models.CharField(max_length= 10, blank = True, null = True)
+	email = models.EmailField()
+
+	def __str__(self):
+		return 'Perfil del usuario {}'.format(self.user_profile.username)
