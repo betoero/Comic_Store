2cd98a21fc6f2d82cb38ab062c0a6f3db2776d97
Jessica13Alonso <jessica.alonsodiaz@gmail.com>
2017-04-07 09:38:45 -0500

    Jessica

--- comics/urls.py
+++ comics/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^list_comics/$', views.ListComics.as_view(), name="list_comics"),
+	url(r'^(?P<category_slug>[-\w]+)/$', views.ListComics.as_view(), name="comics_list_category"),
+	url(r'^(?P<company_slug>[-\w]+)/$', views.ListComics.as_view(), name="comics_list_company"),
 ]
--- comics/views.py
+++ comics/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
 from django.views.generic import View
 from.models import Category, Company, Comic
+from django.utils.decorators import method_decorator
+
 
 # Create your views here.
 class ListComics(View):
-	def get(self, request):
+	def get(self, request, category_slug=None, company_slug=None):
 		template_name = "comics/list_comics.html"
 		categories = Category.objects.all()
 		comics = Comic.objects.all()
+		company = Company.objects.all()
+		if category_slug:
+			category = get_object_or_404(Category, slug=category_slug)
+			comics = comics.filter(category = category)
+		if company_slug:
+			company = get_object_or_404(Company, slug=company_slug)
+			comics = comics.filter(category =  category)
 		context = {
 			'comics':comics,
 			'categories':categories,
+			'company':company,
 		}
 		return render(request, template_name, context)
